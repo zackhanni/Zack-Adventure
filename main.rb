fc87534744b0bcf9a2ed172494ca0f3f32d9ce4f
Zack Hanni <zack.hanni@gmail.com>
2015-01-09 18:59:23 -0500

    code asks for player information (name and quest)

--- main.rb
+++ main.rb
@@ -1,4 +1,9 @@
-puts "hello! What's your name?"
-answer = gets.chomp
-puts "your name is #{answer}"
+puts "You are about to venture out into a magical land, but first we 
+need to learn more about you"
+puts #extra spacing
+puts "What if your name?"
+player_name = gets.chomp
+puts "What is your quest?"
+player_quest = gets.chomp
+puts "Your name is #{player_name} and your quest is #{player_quest}"
 
