d097a286628d09e4fcc8e183eda4c26628eb4c72
Zack Hanni <zack.hanni@gmail.com>
2015-01-10 03:18:58 -0500

    fixed bug with `puts` and `strings` not being on same line

--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
-puts 
-"You are about to venture into a magical land, but first we need to 
+puts "You are about to venture into a magical land, but first we need to 
 learn more about you..."
 puts #=>extra spacing
 puts "What is your name?"
@@ -21,21 +20,19 @@ end
 
 
 enemy_name = player_name.reverse.split.map(&:capitalize).join(' ')
-puts 
-"In the the keep's finest #{player_color} colored kirtle, a most 
+puts "In the the keep's finest #{player_color} colored kirtle, a most 
 unlikely hero embarks on a perilous quest of #{player_quest} in order 
 to destroy the evil #{enemy_name}!"
 
 puts #=>extra spacing
 castle_name = player_color.reverse.split.map(&:capitalize).join(' ')
-puts 
-"#{player_name} is a handmaiden in Castle #{castle_name}. Well... 
+puts "#{player_name} is a handmaiden in Castle #{castle_name}. Well... 
 long story short humans in an alternate reality found out how to make 
 the zombie virus... the hard way"
 puts #=>extra spacing
-puts 
-"Humans in that reality had time machines, so the world counsil came 
-up with a plan to dump them here in your time!"
+puts "Humans in that reality had time machines, so the world counsil came 
+up with a plan to dump them, what your people call the #{enemy_name}s, 
+here in your time!"
 puts #=>extra spacing
 puts "Now it's up to you to save your world!"
 
