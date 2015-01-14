ce2e2aae8b8db3394fb7a5774d0be3b8323bb3a2
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-14 00:00:37 -0500

    Refactored how a player can execute commands

--- lib/room.rb
+++ lib/room.rb
@@ -1,7 +1,19 @@
+require_relative 'support'
+
 class Room
     attr_accessor :entrance_message, :commands
 
-    def initialize(entrance_message, commands)
-        @entrance_message, @commands = entrance_message, commands
+    def prompt_for_command
+        puts "what will you do?"
+        input_command = gets.chomp
+
+        @commands.each do |available_command|
+            if input_command == available_command
+                return self.send(available_command.snakecase)
+            end
+        end
+        # (else) we should only reach this line if we never returned after
+        # finding a matching available command
+        puts "I dont understand. try 'open' or 'search' and a location"
     end
 end
