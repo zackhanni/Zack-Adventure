063044dd6e981be61a5edcb6e1497ce6ed16a343
Zack Hanni <zack.hanni@gmail.com>
2015-01-14 22:05:00 -0500

    Now able to do stuff in room (open/search) 	-unlock chest 		-checks for key 	-go back to first room

--- lib/rooms/large_room.rb
+++ lib/rooms/large_room.rb
@@ -1,6 +1,28 @@
 class LargeRoom < Room
     def initialize
-        @entrance_message = "You enter a much larger room."
-        @commands = []
+        @entrance_message = "You enter a much larger room entierly colored #{player.color}.
+        There is only a locked chest and the door where you entered"
+        @commands = ["open door", "open chest", "search chest", "search room", "unlock chest"]
     end
+
+    def open_door
+        @player.enter_room(SmallRoom.new)
+    end
+
+    def open_chest
+        puts "The chest is locked"
+    end
+
+    def unlock_chest
+    	if @player.has_key?
+        	puts "You unlocked the chest!"
+    	else
+    		puts "You need to find the key first."
+    	end
+    end
+
+    def search_room
+        puts @entrance_message
+    end
+
 end
