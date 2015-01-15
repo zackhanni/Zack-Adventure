9161a277e7fc0bb3b337f126ab0aa8f4fdb95bec
Zack Hanni <zack.hanni@gmail.com>
2015-01-14 22:10:17 -0500

    require 'support' player now prompted for  command after giving input.

--- lib/room.rb
+++ lib/room.rb
@@ -3,17 +3,21 @@ require_relative 'support'
 class Room
     attr_accessor :entrance_message, :commands
 
+    def initialize
+        @player = Player.instance
+    end
+
     def prompt_for_command
         puts "what will you do?"
         input_command = gets.chomp
 
         @commands.each do |available_command|
             if input_command == available_command
-                return self.send(available_command.snakecase)
+                self.send(available_command.snakecase)
+                prompt_for_command
             end
         end
-        # (else) we should only reach this line if we never returned after
-        # finding a matching available command
+
         puts "I dont understand. try 'open' or 'search' and a location"
     end
 end
